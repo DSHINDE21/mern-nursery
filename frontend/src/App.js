5702205f9b69012b3c3a1db8536d08aab89871d6
DSHINDE21 <dinesh.s.shinde1@gmail.com>
2023-01-24 01:03:55 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+// import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/"> nursery</a> {/* root link to app itself  */}
+      </header>
+
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
