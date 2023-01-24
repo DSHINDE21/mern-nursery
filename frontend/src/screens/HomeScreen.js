e2446348a3c904d48e7dd3dc538da04e898deff3
DSHINDE21 <dinesh.s.shinde1@gmail.com>
2023-01-24 19:45:08 +0530

    color-change

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -14,14 +14,14 @@ function HomeScreen() {
               <img src={product.image} alt={product.name} />
             </Link>
 
-            <div className="profuct-info">
+            <div className="product-info">
               <Link to={`/product/${product.slug}`}>
                 <p>{product.name}</p>
               </Link>
               <p>
                 <strong>{product.price}</strong>
               </p>
-              <button>Add to cart</button>
+              <button id="addTocart">Add to cart</button>
             </div>
           </div>
         ))}
