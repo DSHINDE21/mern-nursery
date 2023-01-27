5d0db865d8673c549d42156822feb3f2018e056f
DSHINDE21 <dinesh.s.shinde1@gmail.com>
2023-01-28 02:49:08 +0530

    managing state by reducer hook

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,15 +1,43 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 import { Link } from 'react-router-dom';
 // import data from '../data'; commentinf because we fetching data from backend
 import axios from 'axios';
+import logger from 'use-reducer-logger';
 
+// Reducer function to reduce complexities
+// It Replaces useState
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, products: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 function HomeScreen() {
+  //use reducer
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    products: [], //default value for product
+    loading: true,
+    error: '',
+  });
   // state to fetch
-  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]); this is replace by reducer above
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get('/api/products');
-      setProducts(result.data);
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.get('/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+      }
+      // const result = await axios.get('/api/products');
+      // setProducts(result.data);
     };
     fetchData();
   }, []);
@@ -22,22 +50,28 @@ function HomeScreen() {
       <div className="products">
         {/* Using map function  */}
         {/* data.prosucts.map is removed because we using backenf to fetch data */}
-        {products.map((product) => (
-          <div key={product.slug} className="product">
-            <Link to={`/product/${product.slug}`}>
-              <img src={product.image} alt={product.name} />
-            </Link>
-
-            <div className="product-info">
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          products.map((product) => (
+            <div key={product.slug} className="product">
               <Link to={`/product/${product.slug}`}>
-                <p>{product.category}</p>
+                <img src={product.image} alt={product.name} />
               </Link>
-              <p>{/* <strong>{product.price}</strong> */}</p>
-              {/* <button id="addTocart">Add to cart</button> */}
-              <button id="addTocart">SHOP NOW</button>
+
+              <div className="product-info">
+                <Link to={`/product/${product.slug}`}>
+                  <p>{product.category}</p>
+                </Link>
+                <p>{/* <strong>{product.price}</strong> */}</p>
+                {/* <button id="addTocart">Add to cart</button> */}
+                <button id="addTocart">SHOP NOW</button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
