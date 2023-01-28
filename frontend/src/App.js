87f45e5c9dfecf786c52a48c360683379dc10b7a
DSHINDE21 <dinesh.s.shinde1@gmail.com>
2023-01-28 16:39:43 +0530

    adding react-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,43 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/"> SHOP </Link> {/* root link to app itself  */}
+          <Navbar bg="success" varient="success">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand className="text-white font-weight-xl ">
+                  SHOP
+                </Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+          {/* simple navbar is converted into bootstarp navbar or header above  */}
+          {/* <Link to="/"> SHOP </Link> root link to app itself   */}
         </header>
 
         {/* <main>list products</main> */}
         <main>
-          <Routes>
-            <Route path="/" element={<HomeScreen />} />
-            <Route path="/product/:slug" element={<ProductScreen />} />
-          </Routes>
+          <Container>
+            {/* bootstarp Container  */}
+            <Routes>
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/product/:slug" element={<ProductScreen />} />
+            </Routes>
+          </Container>
         </main>
+
+        {/* Footer starts */}
+        <footer className="text-center bg-success text-white">
+          <div>All rights reserved @2023</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import '../node_modules/bootstrap/dist/css/bootstarp.min.css'; // importing bootstrap
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
